ded90acb49b38de681ef8ef2b1ccdb8842058d73
dmawardi <dmawardi@gmail.com>
2023-01-25 21:47:20 +0800

    Removed close button from modal

--- components/widgets/VideoModal.vue
+++ components/widgets/VideoModal.vue
@@ -20,13 +20,6 @@
             <h5 class="text-xl font-medium leading-normal text-white">
               {{ title }}
             </h5>
-            <!-- Close button -->
-            <button
-              type="button"
-              class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
           </div>
 
           <!-- Body -->
